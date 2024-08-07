aca48d70fe93110ae1ddc36ba619729e50a8eea5
UJARLA MAHITHESH <143686919+Mahithesh@users.noreply.github.com>
2024-08-07 19:57:40 +0530

    Factorial.py
    Write a function to compute the factorial of a given number using a loop. For example, factorial(5) should return 120

---
+++ Factorial.py
@@ -0,0 +1,6 @@
+#Write a function to compute the factorial of a given number using a loop. For example, factorial(5) should return 120
+n = int(input())
+s = 1
+for i in range(1,n+1):
+  s *= i 
+print(s)
